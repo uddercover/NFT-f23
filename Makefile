f2c2ae3105183ff319224f2fb59fad95a76207a5
Uddercover <112282356+UdderlyImprobable@users.noreply.github.com>
2024-07-01 07:10:01 +0100

    Write Interactions scripts For minting and flipping the mood of the NFT

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@
 
 DEFAULT_ANVIL_KEY := 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 
+
 help:
 	@echo "Usage:"
 	@echo "  make deploy [ARGS=...]\n    example: make deploy ARGS=\"--network sepolia\""
@@ -39,10 +40,10 @@ ifeq ($(findstring --network sepolia,$(ARGS)),--network sepolia)
 	NETWORK_ARGS := --rpc-url $(SEPOLIA_RPC_URL) --private-key $(PRIVATE_KEY) --broadcast --verify --etherscan-api-key $(ETHERSCAN_API_KEY) -vvvv
 endif
 
-deploy:
+deployBasicNft:
 	@forge script script/DeployBasicNFT.s.sol:DeployBasicNFT $(NETWORK_ARGS)
 
-mint:
+mintBasicNft:
 	@forge script script/Interactions.s.sol:MintBasicNft ${NETWORK_ARGS}
 
 deployMood:
--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNFT} from "../src/BasicNFT.sol";
+import {MoodNft} from "../src/MoodNft.sol";
 
 contract MintBasicNft is Script {
     string public constant CHIHUAHUA_URI =
@@ -21,5 +22,40 @@ contract MintBasicNft is Script {
         BasicNFT(basicNft).mintNft(CHIHUAHUA_URI);
         vm.stopBroadcast();
         return "Nft Minted";
+    }
+}
+
+contract MintMoodNft is Script {
+    function run() external returns (string memory) {
+        address mostRecentlyDeployedMoodNft = DevOpsTools
+            .get_most_recent_deployment("MoodNft", block.chainid);
+        string memory minted = mint(mostRecentlyDeployedMoodNft);
+        return minted;
+    }
+    //call the mint function on the contract
+    function mint(address moodNft) public returns (string memory) {
+        vm.startBroadcast();
+        MoodNft(moodNft).mintNft();
+        vm.stopBroadcast();
+        return "Mood Nft Minted";
+    }
+}
+
+contract FlipMoodNft is Script {
+    function run() external returns (string memory) {
+        address mostRecentlyDeployedMoodNft = DevOpsTools
+            .get_most_recent_deployment("MoodNft", block.chainid);
+        string memory flipped = flipMood(mostRecentlyDeployedMoodNft, 0);
+        return flipped;
+    }
+    //call the mint function on the contract
+    function flipMood(
+        address moodNft,
+        uint256 tokenId
+    ) public returns (string memory) {
+        vm.startBroadcast();
+        MoodNft(moodNft).flipMood(tokenId);
+        vm.stopBroadcast();
+        return "Mood flipped";
     }
 }
